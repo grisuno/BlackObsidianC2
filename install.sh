f4cb324fc088a86fedaf1b4df4f76ebd723133eb
Grisuno <grisuno@gmail.com>
2025-10-31 01:20:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ go build -o c2-server
 export TLS_CERT=./cert.pem
 export TLS_KEY=./key_go.pem
 export C2_AES_KEY=18547a9428b62fdf2ba11cebc786bccbca8a941748d3acf4aad100ac65d0477f
+./c2-server superuser upsert LazyOwn@local.local LazyOwn
 ./c2-server serve --https=0.0.0.0:4444
