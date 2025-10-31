6fbaa3dc4cdc50e170eedd4d0994942c078ec46a
Grisuno <grisuno@gmail.com>
2025-10-31 01:17:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-python3 -m venv env
-source env/bin/activate
-pip install -r requirements.txt
+go build -o c2-server
+
+# Ejecutar
+export TLS_CERT=./cert.pem
+export TLS_KEY=./key_go.pem
+export C2_AES_KEY=18547a9428b62fdf2ba11cebc786bccbca8a941748d3acf4aad100ac65d0477f
+./c2-server serve --https=0.0.0.0:4444
